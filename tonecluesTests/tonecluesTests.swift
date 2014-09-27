48f9787c3f5fa76fe729f5c632493b80684b05f8
Richard Kmieciak <mr.kmieciak@gmail.com>
2014-09-27 08:02:37 -0700

    setting up

---
+++ tonecluesTests/tonecluesTests.swift
@@ -0,0 +1,36 @@
+//
+//  tonecluesTests.swift
+//  tonecluesTests
+//
+//  Created by Richard Kmieciak on 9/27/14.
+//  Copyright (c) 2014 Subatonic. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class tonecluesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
