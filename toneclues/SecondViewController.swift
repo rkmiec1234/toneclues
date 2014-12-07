cac1f471121538e0b4220f46e4e743ebf2b092da
rkmiec1234 <mr.kmieciak@gmail.com>
2014-12-07 09:37:58 -0800

    piano commit

--- toneclues/SecondViewController.swift
+++ toneclues/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("2ndVC viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
